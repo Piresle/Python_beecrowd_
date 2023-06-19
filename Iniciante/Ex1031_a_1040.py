e28f0898266f583ccd7026ef3ad78faf4c1e6c19
Leticia Pires <piresleticia@yahoo.com>
2023-06-19 16:06:15 -0300

    ex 1040

--- Iniciante/Ex1031_a_1040.py
+++ Iniciante/Ex1031_a_1040.py
@@ -29,10 +29,11 @@ print('1035 - ')
 
 # ----------------------------------------------------------------------------------------------------------------
 print('-'*30)
-print('1036 - ')
+print('1036 - Fórmula de Bhaskara')
 # Leia 3 valores de ponto flutuante (A, B e C) e efetue o cálculo das raízes da equação de Bhaskara. Se não for possível calcular as raízes, mostre a mensagem correspondente “Impossivel calcular”, caso haja uma divisão por 0 ou raiz de numero negativo.
 # Se não houver possibilidade de calcular as raízes, apresente a mensagem "Impossivel calcular". Caso contrário, imprima o resultado das raízes com 5 dígitos após o ponto, com uma mensagem correspondente conforme exemplo abaixo. Imprima sempre o final de linha após cada mensagem.
 
+# ERRO
 from math import sqrt 
 
 A = float(input())
@@ -69,8 +70,31 @@ print('1039 - ')
 
 # ----------------------------------------------------------------------------------------------------------------
 print('-'*30)
-print('1040 - ')
-#
+print('1040 - Média 3')
+# Leia quatro números (N1, N2, N3, N4), cada um deles com uma casa decimal, correspondente às quatro notas de um aluno. Calcule a média com pesos 2, 3, 4 e 1, respectivamente, para cada uma destas notas e mostre esta média acompanhada pela mensagem "Media: ". Se esta média for maior ou igual a 7.0, imprima a mensagem "Aluno aprovado.". Se a média calculada for inferior a 5.0, imprima a mensagem "Aluno reprovado.". Se a média calculada for um valor entre 5.0 e 6.9, inclusive estas, o programa deve imprimir a mensagem "Aluno em exame.".
+# No caso do aluno estar em exame, leia um valor correspondente à nota do exame obtida pelo aluno. Imprima então a mensagem "Nota do exame: " acompanhada pela nota digitada. Recalcule a média (some a pontuação do exame com a média anteriormente calculada e divida por 2). e imprima a mensagem "Aluno aprovado." (caso a média final seja 5.0 ou mais ) ou "Aluno reprovado.", (caso a média tenha ficado 4.9 ou menos). Para estes dois casos (aprovado ou reprovado após ter pego exame) apresente na última linha uma mensagem "Media final: " seguido da média final para esse aluno. Todas as respostas devem ser apresentadas com uma casa decimal. 
+
+# ERRO
+N1 = float(input())
+N2 = float(input())
+N3 = float(input())
+N4 = float(input())
+media = (N1 + N2 + N3 + N4) / 4
+print(f'Media: {media:.1f}')
+if media >= 7.0:
+    print("Aluno aprovado.")
+elif media < 5.0:
+    print("Aluno reprovado.")
+else:
+    print("Aluno em exame.")
+    ex = float(input())
+    mf = (media+ex)/2
+    print(f'Nota do exame: {ex:.1f}')
+    if mf >= 5.0:
+        print("Aluno aprovado.")
+    else:
+        print("Aluno reprovado.")
+    print(f'Media final: {mf:.1f}')
 
 
 # ----------------------------------------------------------------------------------------------------------------
--- Iniciante/testes.py
+++ Iniciante/testes.py
@@ -1,14 +1,38 @@
-from math import sqrt 
-
-A, B, C = map(str(input()).split())
-A = float(A)
-B = float(B)
-C = float(C)
-delta = (B**2) - (4*A*C)
-if A == 0:
-    print("Impossível Calcular")
+N1 = float(input())
+N2 = float(input())
+N3 = float(input())
+N4 = float(input())
+media = (N1 + N2 + N3 + N4) / 4
+print(f'Media: {media:.1f}')
+if media >= 7.0:
+    print("Aluno aprovado.")
+elif media < 5.0:
+    print("Aluno reprovado.")
 else:
-    R1 = (-B + (delta**(1/2))) / (2*A)
-    R2 = (-B - (delta**(1/2))) / (2*A)
-    print(f'R1 = {R1:.5f}')
-    print(f'R2 = {R2:.5f}')
+    print("Aluno em exame.")
+    ex = float(input())
+    mf = (media+ex)/2
+    print(f'Nota do exame: {ex:.1f}')
+    if mf >= 5.0:
+        print("Aluno aprovado.")
+    else:
+        print("Aluno reprovado.")
+    print(f'Media final: {mf:.1f}')
+
+
+
+'''
+ Se esta média for maior ou igual a 7.0, imprima a mensagem "Aluno aprovado.". Se a média calculada for inferior a 5.0, imprima a mensagem "Aluno reprovado.". Se a média calculada for um valor entre 5.0 e 6.9, inclusive estas, o programa deve imprimir a mensagem "Aluno em exame.".
+
+No caso do aluno estar em exame, leia um valor correspondente à nota do exame obtida pelo aluno. Imprima então a mensagem "Nota do exame: " acompanhada pela nota digitada. Recalcule a média (some a pontuação do exame com a média anteriormente calculada e divida por 2). e imprima a mensagem "Aluno aprovado." (caso a média final seja 5.0 ou mais ) ou "Aluno reprovado.", (caso a média tenha ficado 4.9 ou menos). Para estes dois casos (aprovado ou reprovado após ter pego exame) apresente na última linha uma mensagem "Media final: " seguido da média final para esse aluno.'''
+
+'''
+2.0 4.0 7.5 8.0
+6.4
+
+Media: 5.4
+Aluno em exame.
+Nota do exame: 6.4
+Aluno aprovado.
+Media final: 5.9
+'''
