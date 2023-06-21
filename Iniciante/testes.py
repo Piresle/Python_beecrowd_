c3b9386127432fff1b372253cef0d2e485cb46bf
Leticia Pires <piresleticia@yahoo.com>
2023-06-21 18:12:41 -0300

    1042

---
+++ Iniciante/testes.py
@@ -0,0 +1,11 @@
+valores = list(map(int, input().split())) # digitar todos os valores na mesma linha 
+print(sorted(valores))
+
+for valor in sorted(valores):
+    print(valor)
+
+print("")
+
+for valor in valores:
+    print(valor)
+
