5ab2009ec3fbe3855c5f1811ca322b2165f1a43d
Leticia Pires <piresleticia@yahoo.com>
2023-06-26 11:30:09 -0300

    1050

--- Iniciante/Ex1041_a_1050.py
+++ Iniciante/Ex1041_a_1050.py
@@ -82,9 +82,36 @@ print('1049 - ')
 
 # ----------------------------------------------------------------------------------------------------------------
 print('-'*30)
-print('1050 - ')
-#
+print('1050 - DDD')
+#Leia um número inteiro que representa um código de DDD para discagem interurbana. Em seguida, informe à qual cidade o DDD pertence, considerando a tabela abaixo:
+# 61 - Brasília 
+# 71 - Salvador
+# 11 - São Paulo
+# 21 - Rio de Janeiro
+# 32 - Juiz de Fora
+# 19 - Campinas 
+# 17 - Vitoria
+# 31 - Belo Horizonte
+# Se a entrada for qualquer outro DDD que não esteja presente na tabela acima, o programa deverá informar: "DDD nao cadastrado"
 
-
+ddd = int(input())
+if ddd == 61:
+    print('Brasilia')
+elif ddd == 71:
+    print('Salvador')
+elif ddd == 11:
+    print('Sao Paulo')
+elif ddd == 21:
+    print('Rio de Janeiro')
+elif ddd == 32:
+    print('Juiz de Fora')
+elif ddd == 19:
+    print('Campinas')
+elif ddd == 27:
+    print('Vitoria')
+elif ddd == 31:
+    print('Belo Horizonte')
+else:
+    print('DDD nao cadastrado')
 # ----------------------------------------------------------------------------------------------------------------
 
