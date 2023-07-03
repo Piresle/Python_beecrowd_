c9a603b8c50c140b26c773d71002626097c84f78
Leticia Pires <piresleticia@yahoo.com>
2023-07-03 10:39:55 -0300

    1035

--- Iniciante/Ex1031_a_1040.py
+++ Iniciante/Ex1031_a_1040.py
@@ -24,8 +24,16 @@ print('1034 - ')
 # ----------------------------------------------------------------------------------------------------------------
 print('-'*30)
 print('1035 - ')
-#
-
+# Leia 4 valores inteiros A, B, C e D. A seguir, se B for maior do que C e se D for maior do que A, e a soma de C com D for maior que a soma de A e B e se C e D, ambos, forem positivos e se a variável A for par escrever a mensagem "Valores aceitos", senão escrever "Valores nao aceitos".
+entrada = input().split()
+A = int(entrada[0])
+B = int(entrada[1])
+C = int(entrada[2])
+D = int(entrada[3])
+if (B > C) and (D > A) and ((C+D) > (A+B)) and (C > 0) and (D > 0) and ((A%2) == 0):
+    print("Valores aceitos")
+else:
+    print("Valores nao aceitos")
 
 # ----------------------------------------------------------------------------------------------------------------
 print('-'*30)
