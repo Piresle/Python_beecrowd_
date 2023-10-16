f2c812d02f46af19f24681ad0ba1c85f4e17f199
Leticia Pires <piresleticia@yahoo.com>
2023-10-16 10:22:31 -0300

    1000 a 1010

--- Iniciante/testes.py
+++ Iniciante/testes.py
@@ -1,16 +1,14 @@
-# Leia 3 valores reais (A, B e C) e verifique se eles formam ou não um triângulo. Em caso positivo, calcule o perímetro do triângulo e apresente a mensagem:
-# Perimetro = XX.X
-# Em caso negativo, calcule a área do trapézio que tem A e B como base e C como altura, mostrando a mensagem
-# Area = XX.X
 
-a,b,c = input().split()
-A = float(a)
-B = float(b)
-C = float(c)
-if ((A+B) < C) and ((A+C < B)) and ((B+C) < A):
-    perimetro = A + B + C 
-    print(f'Perimetro = {perimetro:.1f}')
-else:
-    area = (A+B) * C / 2
-    print(f'Area = {area:.1f}')
-# a soma de dois lados é sempre menor que o terceiro lado.
+# ---------------------------------------------------------------------- 
+"""
+Ex1000_a_1010 *
+Ex1011_a_1020
+Ex1021_a_1030
+Ex1031_a_1040
+Ex1041_a_1050
+Ex1051_a_1060
+Ex1061_a_1070
+Ex1071_a_1080
+Ex1081_a_1090
+Ex1091_a_1100
+"""
