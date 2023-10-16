5758a4568fe96dbc21e608f0aa034296c1eb13bc
Leticia Pires <piresleticia@yahoo.com>
2023-10-16 18:00:03 -0300

    1049

--- Iniciante/Ex1041_a_1050.py
+++ Iniciante/Ex1041_a_1050.py
@@ -144,8 +144,48 @@ print('1048 - ')
 
 # ----------------------------------------------------------------------------------------------------------------
 print('-'*30)
-print('1049 - ')
-#
+print('1049 - Animal')
+# Neste problema, você deverá ler 3 palavras que definem o tipo de animal possível segundo o esquema abaixo, da esquerda para a direita.  Em seguida conclua qual dos animais seguintes foi escolhido, através das três palavras fornecidas.
+"""
+vertebrado - ave      - carnivoro - aguia
+                      - onivoro - pomba
+
+           - mamifero - onivoro - homem
+                      - herbivoro - vaca
+
+invertebrado - inseto   - hematofago - pulga
+                        - herbivoro - lagarta 
+
+             - anelideo - hematofago - sanguessuga
+                        - onivoro - minhoca
+"""
+
+a = str(input())
+b = str(input())
+c = str(input())
+if a == 'vertebrado':
+    if b == 'ave':
+        if c == 'carnivoro':
+            print('aguia')
+        elif c == 'onivoro':
+            print('pomba')
+    elif b == 'mamifero': 
+        if c == 'onivoro':
+            print('homem')
+        elif c == 'herbivoro':
+            print('vaca')
+
+if a == 'invertebrado':
+    if b == 'inseto':
+        if c == 'hematofago':
+            print('pulga')
+        elif c == 'herbivoro': 
+            print('lagarta')
+    elif b == 'anelideo':
+        if c == 'hematofago':
+            print('sanguessuga')
+        elif c == 'onivoro':
+            print('minhoca')
 
 
 # ----------------------------------------------------------------------------------------------------------------
