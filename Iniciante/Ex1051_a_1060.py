d081a5d76e97e38ef25dc76eced9722946d49a21
Leticia Pires <piresleticia@yahoo.com>
2023-10-16 12:13:53 -0300

    1052

--- Iniciante/Ex1051_a_1060.py
+++ Iniciante/Ex1051_a_1060.py
@@ -5,8 +5,34 @@ print('1051 - ')
 
 # ----------------------------------------------------------------------------------------------------------------
 print('-'*30)
-print('1052 - ')
-#
+print('1052 - Mês')
+# Leia um valor inteiro entre 1 e 12, inclusive. Correspondente a este valor, deve ser apresentado como resposta o mês do ano por extenso, em inglês, com a primeira letra maiúscula.
+
+N = int(input())
+if N == 1:
+    print('January')
+elif N == 2:
+    print('February')
+elif N == 3:
+    print('March')
+elif N == 4:
+    print('April')
+elif N == 5:
+    print('May')
+elif N == 6:
+    print('June')
+elif N == 7:
+    print('July')
+elif N == 8:
+    print('August')
+elif N == 9:
+    print('September')
+elif N == 10:
+    print('October')
+elif N == 11: 
+    print('November')
+elif N == 12: 
+    print('December')
 
 
 # ----------------------------------------------------------------------------------------------------------------
