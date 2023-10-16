4d6087f414069ef4165ead43234d85a69f13c45d
Leticia Pires <piresleticia@yahoo.com>
2023-10-16 17:25:06 -0300

    1038

--- Iniciante/Ex1031_a_1040.py
+++ Iniciante/Ex1031_a_1040.py
@@ -94,8 +94,32 @@ else:
 
 # ----------------------------------------------------------------------------------------------------------------
 print('-'*30)
-print('1038 - ')
-# 
+print('1038 - Lanche')
+# Com base na tabela abaixo, escreva um programa que leia o código de um item e a quantidade deste item. A seguir, calcule e mostre o valor da conta a pagar.
+"""
+CÓDIGO  ESPECIFICAÇÃO     PREÇO
+1       Cachorro Quente   R$4,00
+2       X - Salada        R$4,50
+3       X - Bacon         R$5,00
+4       Torrada Simples   R$2,00
+5       Refrigerante      R$1,50"""
+
+a, b = map(int, input().split())
+if a == 1:
+    preco = 4.00
+    print(f'Total: R$ {b*preco:.2f}')
+elif a == 2:
+    preco = 4.50
+    print(f'Total: R$ {b*preco:.2f}')
+elif a == 3:
+    preco = 5.00
+    print(f'Total: R$ {b*preco:.2f}')
+elif a == 4:
+    preco = 2.00
+    print(f'Total: R$ {b*preco:.2f}')
+elif a == 5:
+    preco = 1.50
+    print(f'Total: R$ {b*preco:.2f}')
 
 
 # ----------------------------------------------------------------------------------------------------------------
