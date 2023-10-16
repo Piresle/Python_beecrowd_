083f40e91c1086b1d66a18aee7c3195ba08c0f76
Leticia Pires <piresleticia@yahoo.com>
2023-10-16 10:40:13 -0300

    1040

--- Iniciante/Ex1031_a_1040.py
+++ Iniciante/Ex1031_a_1040.py
@@ -96,27 +96,24 @@ print('1040 - Média 3')
 # Leia quatro números (N1, N2, N3, N4), cada um deles com uma casa decimal, correspondente às quatro notas de um aluno. Calcule a média com pesos 2, 3, 4 e 1, respectivamente, para cada uma destas notas e mostre esta média acompanhada pela mensagem "Media: ". Se esta média for maior ou igual a 7.0, imprima a mensagem "Aluno aprovado.". Se a média calculada for inferior a 5.0, imprima a mensagem "Aluno reprovado.". Se a média calculada for um valor entre 5.0 e 6.9, inclusive estas, o programa deve imprimir a mensagem "Aluno em exame.".
 # No caso do aluno estar em exame, leia um valor correspondente à nota do exame obtida pelo aluno. Imprima então a mensagem "Nota do exame: " acompanhada pela nota digitada. Recalcule a média (some a pontuação do exame com a média anteriormente calculada e divida por 2). e imprima a mensagem "Aluno aprovado." (caso a média final seja 5.0 ou mais ) ou "Aluno reprovado.", (caso a média tenha ficado 4.9 ou menos). Para estes dois casos (aprovado ou reprovado após ter pego exame) apresente na última linha uma mensagem "Media final: " seguido da média final para esse aluno. Todas as respostas devem ser apresentadas com uma casa decimal. 
 
-n1, n2, n3, n4 = input().split()
-N1 = float(n1)
-N2 = float(n2)
-N3 = float(n3)
-N4 = float(n4)
-media = ((n1*2) + (n2*3) + (n3*4) + (n4*1)) / 10
-print(f'Media: {media:.1f}')
-if media >= 7.0:
+A,B,C,D = map(float,input().split())
+A = (A*2+B*3+C*4+D*1)/10
+print(f'Media: {A:.1f}')
+if A>=7.0:
     print("Aluno aprovado.")
-elif media < 5.0:
+elif A<5.0:
     print("Aluno reprovado.")
-elif (media>=5) and (media<7):
+elif A>=5.0 and A<7.0:
     print("Aluno em exame.")
-    ex = float(input())
-    print(f'Nota do exame: {ex:.1f}')
-    mf = (media+ex)/2
-    if mf >= 5.0:
+    N = float(input())
+    print(f'Nota do exame: {N:.1f}')
+    N = (A+N)/2
+    if N>=5.0:
         print("Aluno aprovado.")
+        print(f'Media final: {N:.1f}')
     else:
         print("Aluno reprovado.")
-    print(f'Media final: {mf:.1f}')
+        print(f'Media final: {N:.1f}')
 
 # ERRO
 '''
