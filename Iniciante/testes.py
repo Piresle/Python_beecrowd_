4e96c3193c269341030ee8405130122332f43e8e
Leticia Pires <piresleticia@yahoo.com>
2023-10-17 10:49:09 -0300

    1017

--- Iniciante/testes.py
+++ Iniciante/testes.py
@@ -1,14 +1,49 @@
 """
 Ex1000_a_1010 *
-Ex1011_a_1020 - 17, 18, 19, 20
+Ex1011_a_1020 - 17, 18
 Ex1021_a_1030 - 21, 22, 23, 24, 25, 26, 27, 28, 29, 30
 Ex1031_a_1040 - 31, 32, 33, 34, 39
 Ex1041_a_1050 - 41, 43, 46, 47, 48
 Ex1051_a_1060 - 51, 53, 54, 55, 56, 57, 58
 Ex1061_a_1070 - 61, 62, 63, 67, 68, 69, 70
-Ex1071_a_1080 - 71, 73, 74, 75, 76, 77, 79, 80
+Ex1071_a_1080 - 71, 74, 75, 76, 77, 80
 Ex1081_a_1090 - 81, 82, 83, 84, 85, 86, 87, 88, 89, 90
-Ex1091_a_1099 - 91, 92, 93, 94, 95, 96, 97, 98, 99
+Ex1091_a_1099 - 91, 92, 93, 94, 96, 97, 98, 99
 """
 # ----------------------------------------------------------------------------------------------------------------
+
+# Leia um valor inteiro, que é o tempo de duração em segundos de um determinado evento em uma fábrica, e informe-o expresso no formato horas:minutos:segundos. 1019
+
+#N = int(input())
+
+
+
+
+# Leia um valor inteiro N. Apresente o quadrado de cada um dos valores pares, de 1 até N, inclusive N, se for o caso. 1073
+"""
+N = int(input())
+for i in range(N):
+    if N % 2 == 0:
+        print('')"""
+
+# Você deve fazer um programa que apresente a sequencia conforme o exemplo abaixo. 1095
+"""
+Exemplo de Saída
+I=1 J=60
+I=4 J=55
+I=7 J=50
+...
+I=? J=0"""
+
+
+
+# Leia 1 valor inteiro N, que representa o número de casos de teste que vem a seguir. Cada caso de teste consiste de 3 valores reais, cada um deles com uma casa decimal. Apresente a média ponderada para cada um destes conjuntos de 3 valores, sendo que o primeiro valor tem peso 2, o segundo valor tem peso 3 e o terceiro valor tem peso 5. 1079
+
+
+
+
+
+# Leia um valor inteiro correspondente à idade de uma pessoa em dias e informe-a em anos, meses e dias
+# Obs.: apenas para facilitar o cálculo, considere todo ano com 365 dias e todo mês com 30 dias. Nos casos de teste nunca haverá uma situação que permite 12 meses e alguns dias, como 360, 363 ou 364. Este é apenas um exercício com objetivo de testar raciocínio matemático simples. 1020
+
 
